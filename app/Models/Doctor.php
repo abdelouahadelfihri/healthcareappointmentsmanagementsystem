566a979b2ddc1f7166ca776a7515328588e85a18
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-30 22:07:20 +0100

    updates to Doctor.php

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'specialty',
+        'phone',
+        'email'
+    ];
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+
 }
