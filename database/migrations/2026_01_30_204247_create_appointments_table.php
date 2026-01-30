88788461680c82ef744b96969d9fb13fa07249b3
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-30 21:59:17 +0100

    updates to 2026_01_30_204247_create_appointments_table.php

--- database/migrations/2026_01_30_204247_create_appointments_table.php
+++ database/migrations/2026_01_30_204247_create_appointments_table.php
@@ -4,22 +4,34 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('patient_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('doctor_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->dateTime('appointment_datetime');
+
+            $table->enum('status', [
+                'scheduled',
+                'completed',
+                'cancelled'
+            ])->default('scheduled');
+
+            $table->text('notes')->nullable();
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('appointments');
