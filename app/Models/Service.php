34e32237ea939a038a0dc32cc2f7999a1a3ca33d
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-30 22:09:16 +0100

    updates to Service.php

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price'
+    ];
+
+    public function appointments()
+    {
+        return $this->belongsToMany(
+            Appointment::class,
+            'appointment_services'
+        )->withPivot(['quantity', 'unit_price'])
+            ->withTimestamps();
+    }
 }
