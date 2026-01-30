6a45eac493f54e1f9eaad57ca4ef9cdc3ac952fb
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-01-30 21:54:53 +0100

    updates to 2026_01_30_204233_create_services_table.php

--- database/migrations/2026_01_30_204233_create_services_table.php
+++ database/migrations/2026_01_30_204233_create_services_table.php
@@ -4,22 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2)->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('services');
