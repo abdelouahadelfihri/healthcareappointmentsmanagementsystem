371af3b50a0045200866179ae0ff7aa463ed822f
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-02-15 10:27:41 +0000

    updates to Appointment.phpp

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+
     protected $fillable = [
         'patient_id',
         'doctor_id',
@@ -16,23 +17,23 @@ class Appointment extends Model
         'notes'
     ];
 
+    // Relation to patient
     public function patient()
     {
         return $this->belongsTo(Patient::class);
     }
 
+    // Relation to doctor
     public function doctor()
     {
         return $this->belongsTo(Doctor::class);
     }
 
+    // Relation to services (pivot table)
     public function services()
     {
-        return $this->belongsToMany(
-            Service::class,
-            'appointment_services'
-        )->withPivot(['quantity', 'unit_price'])
+        return $this->belongsToMany(Service::class, 'appointment_services')
+            ->withPivot(['quantity', 'unit_price']) // if you want extra fields
             ->withTimestamps();
     }
-
 }
