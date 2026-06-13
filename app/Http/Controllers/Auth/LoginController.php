4e2de2b50173a6c7d5c00614714df34bc45c2852
Abdelouahad <abdelouahadelfihri107@gmail.com>
2026-06-13 18:56:05 +0100

    updates to LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,7 +31,8 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('dashboard');
+            return redirect()->route('dashboard')
+                - with('success', 'Logged in successfully!');
         }
 
         return back()->withErrors([
